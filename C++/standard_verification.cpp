0ff09437398bacb6903f020d7774ba9b5c9b1367
torres98 <lucatorresetti@hotmail.it>
2022-05-01 14:16:58 +0200

    The standard verification procedure is now lazy, it stops computing as soon as it finds an invalid result

--- C++/standard_verification.cpp
+++ C++/standard_verification.cpp
@@ -5,7 +5,27 @@
 
 template <typename Element>
 bool verify_signature(Matrix<Element> &M, Vector<Element> &v) {
-    return (M * v).is_zero();
+
+    if (M.columns != v.rows) {
+        std::ostringstream error_log;
+        error_log << "Incompatible sizes for signature verification (matrix of size " << M.rows << "x" << M.columns << " and vector of size " << v.rows << "x" << v.columns << ")";
+        throw std::invalid_argument(error_log.str());
+    }
+
+    Element cell;
+
+    for (unsigned int i = 0; i < M.rows; i++) {
+        cell = Element();
+
+        for (unsigned int j = 0; j < v.rows; j++)
+                cell += M[i][j] * v[j];
+
+        if (cell)
+            return false;
+
+    }
+
+    return true;
 }
 
 #endif

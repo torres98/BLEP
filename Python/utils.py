61b2431ed3b844efbf1fb1cce8094ef4ce28df72
torres98 <lucatorresetti@hotmail.it>
2022-05-25 10:19:58 +0200

    Added function to fill a numpy array randomly

--- Python/utils.py
+++ Python/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+from random import randint
+
 def wordlist_to_int(wordlist, word_size, endianess = 'big'):
     value = 0
 
@@ -9,3 +12,11 @@ def wordlist_to_int(wordlist, word_size, endianess = 'big'):
             value |= wordlist[i] << (word_size * (len(wordlist) - i - 1))
 
     return value
+
+def fill_matrix_randomly(M, min_value, max_value, elem_type):
+    M_2d = np.atleast_2d(M)
+    n, m = M_2d.shape
+
+    for i in range(n):
+        for j in range(m):
+            M_2d[i, j] = elem_type(randint(min_value, max_value - 1))

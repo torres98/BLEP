a579f35dc2ef425a3e134e889c5d3b412822f0aa
torres98 <lucatorresetti@hotmail.it>
2022-06-12 18:02:47 +0200

    Updated Matrix and Vector class, fixed some issues

--- c++/include/matrix.h
+++ c++/include/matrix.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <sstream>
@@ -9,47 +10,41 @@ template <typename Element>
 class Matrix {
     protected:
         Element* M;
-        unsigned int rows, columns;
+        uint16_t rows, columns;
 
     public:
         
-        Matrix(unsigned int rows, unsigned int columns, bool init = false) {
-
+        Matrix(uint16_t rows, uint16_t columns, bool init = false) {
             this -> rows = rows;
             this -> columns = columns;
 
             if (init)
-                M = new Element[rows * columns]();
+                this -> M = new Element[rows * columns]();
             else
-                M = new Element[rows * columns];
-
+                this -> M = new Element[rows * columns];
         }
 
         //Constructor from static 2D array
-        template <unsigned int rows, unsigned int columns>
-        Matrix(const Element (&A)[rows][columns]) {
-
+        template <uint16_t rows, uint16_t columns>
+        Matrix(const Element (&array)[rows][columns]) {
             this -> rows = rows;
             this -> columns = columns;
-            M = new Element[rows * columns];
+            this -> M = new Element[rows * columns];
 
-            memcpy(this -> M, A, rows * columns * sizeof(Element)); //FIX: could raise problems with objects
-
+            std::copy(*array, *array + rows * columns, this -> M);
         }
 
         // Copy constructor
         Matrix(const Matrix& A) {
+            this -> rows = A.rows;
+            this -> columns = A.columns;
+            this -> M = new Element[rows * columns];
 
-            rows = A.rows;
-            columns = A.columns;
-            M = new Element[rows * columns];
-
-            memcpy(this -> M, A.M, rows * columns * sizeof(Element)); //FIX: could raise problems with objects
-
+            std::copy(A.M, A.M + rows * columns, this -> M);
         }
 
         // Slice constructor
-        Matrix(const Matrix& A, unsigned int row_i, unsigned int row_f, unsigned int col_i, unsigned int col_f) {
+        Matrix(const Matrix& A, uint16_t row_i, uint16_t row_f, uint16_t col_i, uint16_t col_f) {
 
             if (row_i >= A.rows || row_f > A.rows) {
                 std::stringstream error_log;
@@ -61,15 +56,15 @@ class Matrix {
                 throw std::invalid_argument(error_log.str());
             }
 
-            rows = row_f - row_i;
-            columns = col_f - col_i;
-            M = new Element[rows * columns];
+            this -> rows = row_f - row_i;
+            this -> columns = col_f - col_i;
+            this -> M = new Element[rows * columns];
 
-            if (columns == A.columns) {
-                memcpy(this -> M, A[row_i], rows * columns * sizeof(Element)); //FIX: could raise problems with objects
+            if (this -> columns == A.columns) {
+                std::copy(A.M + row_i * A.columns, A.M + row_f * A.columns, this -> M);
             } else {
-                for (unsigned int i = row_i; i < row_f; i++)
-                    memcpy((*this)[i-row_i], &(A[i][col_i]), columns * sizeof(Element)); //FIX: could raise problems with objects
+                for (uint16_t i = row_i; i < row_f; i++)
+                    std::copy(A.M + i * A.columns + col_i, A.M + i * A.columns + col_f, this -> M + (i-row_i) * this -> columns);
             }
 
         }
@@ -78,196 +73,168 @@ class Matrix {
         Matrix() {
             this -> rows = 0;
             this -> columns = 0;
-            M = nullptr;
+            this -> M = nullptr;
         }
 
-        unsigned int nrows() const {
-            return rows;
+        uint16_t nrows() const {
+            return this -> rows;
         }
 
-        unsigned int ncolumns() const {
-            return columns;
+        uint16_t ncolumns() const {
+            return this -> columns;
         }
 
-        static void swap_row(Matrix &M, unsigned int row_1, unsigned int row_2) {
-            Element tmp;
+        uint16_t size() const {
+            return this -> rows * this -> columns;
+        }
 
-            for (unsigned int j = 0; j < M.columns; j++) {
-                tmp = M[row_1][j];
-
-                M[row_1][j] = M[row_2][j];
-                M[row_2][j] = tmp;
-            }
+        void swap_row(uint16_t row_1, uint16_t row_2) {
+            std::swap_ranges(
+                this -> M + row_1 * this -> columns,
+                this -> M + (row_1+1) * this -> columns,
+                this -> M + row_2 * this -> columns
+            );
         }
 
         bool has_full_rank() {
-
             Matrix M_ref = Matrix(*this);
 
             Element pivot;
-            unsigned int pivot_row;
+            uint16_t pivot_row;
 
-            for (unsigned int k = 0; k < rows; k++) {
+            for (uint16_t k = 0; k < M_ref.rows; k++) {
                 // Initialize pivot row index and pivot value
-                pivot = M_ref[k][k];
+                pivot = M_ref(k, k);
                 pivot_row = k;
                 
                 // look for maximum (absolute) pivot value on column k (after row k)
-                for (unsigned int i = k+1; i < rows; i++)
-                    if (M_ref[i][k] > pivot) //should be abs
-                        pivot = M_ref[i][k], pivot_row = i;
+                for (uint16_t i = k+1; i < M_ref.rows; i++)
+                    if (M_ref(i, k) > pivot) //should be abs
+                        pivot = M_ref(i, k), pivot_row = i;
         
                 // if a principal diagonal element is zero, then the
                 // matrix is singular, so it can't have full rank
-                if (!M_ref[k][pivot_row])
+                if (!M_ref(k, pivot_row))
                     return false;
         
                 // Swap the pivot row with current row
                 if (pivot_row != k)
-                    swap_row(M_ref, k, pivot_row);
+                    M_ref.swap_row(k, pivot_row);
 
-                for (unsigned int i = k+1; i < rows; i++) {
+                for (uint16_t i = k+1; i < M_ref.rows; i++) {
                     // current_row = pivot * current_row - current_row[0] * pivot_row
-                    for (unsigned int j = k+1; j < columns; j++)
-                        M_ref[i][j] = pivot * M_ref[i][j] - M_ref[i][k] * M_ref[k][j];
+                    for (uint16_t j = k+1; j < M_ref.columns; j++)
+                        M_ref(i, j) = pivot * M_ref(i, j) - M_ref(i, k) * M_ref(k, j);
         
                     // set to zero the current column value
-                    M_ref[i][k] = Element();
+                    M_ref(i, k) = Element();
                 }
 
             }
-
-            std::cout << M_ref;
+            
             return true;
         }
 
-        /*bool has_full_rank_v2() {
-
-            Matrix M_ref = Matrix(*this);
-
-            Element pivot, f;
-            unsigned int pivot_row;
-
-            unsigned int row_indices = new unsigned int[M_ref.rows];
-            
-            for (unsigned int i = 0; i < M_ref.rows; i++)
-                row_indices[i] = i;
-
-            for (unsigned int k = 0; k < rows; k++) {
-                // Initialize maximum value and index for pivot
-                pivot_row = row_indices[k];
-                pivot = M_ref[pivot_row][pivot_row];
-
-                // look for maximum (absolute) pivot value on column k (after row k)
-                for (unsigned int i = k+1; i < rows; i++)
-                    if (M_ref[i][k] > pivot) //should be abs
-                        pivot = M_ref[i][k], pivot_row = i;
-        
-                // if a principal diagonal element is zero, then the matrix is
-                // singular, so it can't have full rank
-                if (!M_ref[k][pivot_row])
-                    return false;
-        
-                // Swap the pivot row with current row
-                if (pivot_row != k)
-                    swap_row(M_ref, k, pivot_row);
-
-                for (unsigned int i = k+1; i < rows; i++) {
-                    // factor f to set current row kth element to 0,
-                    // and subsequently remaining kth column to 0
-                    f = M_ref[i][k] / pivot; //works only if division doesn't produce a different type
-        
-                    // subtract fth multiple of corresponding kth row element
-                    for (unsigned int j = k+1; j < columns; j++)
-                        M_ref[i][j] = M_ref[i][j] - M_ref[k][j] * f;
-        
-                    // filling lower triangular matrix with zeros
-                    M_ref[i][k] = Element();
-                }
-
+        //getter
+        Element operator() (uint16_t i, uint16_t j) const { 
+            if (i >= this -> rows) {
+                std::ostringstream error_log;
+                error_log << "Row index " << i << " out of bounds for matrix of size " << this -> rows << "x" << this -> columns;
+                throw std::invalid_argument(error_log.str());
             }
 
-            std::cout << M_ref;
-            return true;
-        }*/
+            if (j >= this -> columns) {
+                std::ostringstream error_log;
+                error_log << "Column index " << j << " out of bounds for matrix of size " << this -> rows << "x" << this -> columns;
+                throw std::invalid_argument(error_log.str());
+            }
 
-        //Seems like i won't be needing it
-        /*Element* operator[](unsigned int i) {
-            return M + i*columns;
-        }*/
+            return (this -> M)[i*columns + j];
+        }
 
-        Element* operator[](unsigned int i) const {
-            return M + i*columns;
+        //setter (tofix)
+        Element& operator() (uint16_t i, uint16_t j) {
+            if (i >= this -> rows) {
+                std::ostringstream error_log;
+                error_log << "Row index " << i << " out of bounds for matrix of size " << this -> rows << "x" << this -> columns;
+                throw std::invalid_argument(error_log.str());
+            }
+
+            if (j >= this -> columns) {
+                std::ostringstream error_log;
+                error_log << "Column index " << j << " out of bounds for matrix of size " << this -> rows << "x" << this -> columns;
+                throw std::invalid_argument(error_log.str());
+            }
+
+            return (this -> M)[i*columns + j];
         }
 
         Matrix operator*(const Matrix &A) const {
             
-            if (columns != A.rows) {
+            if (this -> columns != A.rows) {
                 std::ostringstream error_log;
-                error_log << "Incompatible sizes for product between matrices of size " << rows << "x" << columns << " and " << A.rows << "x" << A.columns;
+                error_log << "Incompatible sizes for product between matrices of size " << this -> rows << "x" << this -> columns << " and " << A.rows << "x" << A.columns;
                 throw std::invalid_argument(error_log.str());
             }
 
-            Matrix O = Matrix(rows, A.columns, true);
+            Matrix O = Matrix(this -> rows, A.columns, true);
             
-            for (unsigned int i = 0; i < rows; i++)
-                for (unsigned int j = 0; j < A.columns; j++)
-                    for (unsigned int h = 0; h < columns; h++)
-                        O[i][j] += (*this)[i][h] * A[h][j];
+            for (uint16_t i = 0; i < this -> rows; i++)
+                for (uint16_t j = 0; j < A.columns; j++)
+                    for (uint16_t h = 0; h < this -> columns; h++)
+                        O.M[i*O.columns + j] += (this -> M)[i*this -> columns + h] * A.M[h*A.columns + j];
 
             return O;
         }
 
         Matrix& operator=(const Matrix &A) {
 
-            if (rows != A.rows || columns != A.columns) {
-                delete[] M;
+            if (this -> rows != A.rows || this -> columns != A.columns) {
+                delete[] this -> M;
 
-                rows = A.rows;
-                columns = A.columns;
-                M = new Element[rows * columns];
+                this -> rows = A.rows;
+                this -> columns = A.columns;
+                this -> M = new Element[this -> rows * this -> columns];
             }
 
-            memcpy(this -> M, A.M, rows * columns * sizeof(Element)); //FIX: could raise problems with objects
+            std::copy(A.M, A.M + rows * columns, this -> M);
 
             return *this;
 
         }
 
         bool operator==(const Matrix &A) const {
-            if (rows != A.rows || columns != A.columns)
+            if (this -> rows != A.rows || this -> columns != A.columns)
                 return false;
 
-            return memcmp(this -> M, A.M, rows * columns * sizeof(Element)) == 0; //FIX: could raise problems with objects
+            return std::equal(this -> M, this -> M + rows * columns, A.M);
         }
 
         ~Matrix() {
-            delete[] M;
+            delete[] this -> M;
+        }
+
+        friend std::ostream& operator<<(std::ostream &s, const Matrix<Element> &A) {
+            uint16_t n = A.rows, m = A.columns;
+
+            if (n == 0 || m == 0) {
+                s << "[]" << std::endl;
+            } else {
+
+                for (uint16_t i = 0; i < n; i++) {
+                    s << "[";
+
+                    for (uint16_t j = 0; j + 1 < m; j++)
+                        s << A.M[i*m + j] << ", ";
+
+                    s << A.M[(i+1)*m - 1] << "]" << std::endl;
+                }
+
+            }
+
+            return s;
         }
 
 };
-
-template <typename Element>
-std::ostream& operator<<(std::ostream &s, const Matrix<Element> &A) {
-
-    unsigned int n = A.nrows(), m = A.ncolumns();
-
-    if (n == 0 || m == 0) {
-        s << "[]";
-    } else {
-
-        for (unsigned int i = 0; i < n; i++) {
-            s << "[";
-
-            for (unsigned int j = 0; j < m-1; j++)
-                s << A[i][j] << ", ";
-
-            s << A[i][m-1] << "]" << std::endl;
-        }
-
-    }
-
-    return s;
-}
 
 #endif
--- c++/include/vector.h
+++ c++/include/vector.h
@@ -12,82 +12,73 @@ template <typename Element>
 class Vector: public Matrix<Element> {
 
     public:
-        Vector(unsigned int size, bool row_vector = false, bool init = false) :
-            Matrix<Element>(row_vector ? 1: size, row_vector ? size: 1, init) {}
+        Vector(uint16_t size, bool row_vector = false, bool init = false) :
+            Matrix<Element>(row_vector ? 1: size, row_vector ? size: 1, init) {} //maybe fix zero size
 
-        Vector(const Matrix<Element> &A) : Matrix<Element>(A) {}
+        Vector(const Matrix<Element> &A) : 
+            Matrix<Element>(A) {}
 
         //Constructor from matrix row
-        Vector(const Matrix<Element> &A, unsigned int row) : Matrix<Element>(1, A.ncolumns()) {
-            if (row >= A.nrows()) {
-                std::stringstream error_log;
-                error_log << "Row index " << row << " out of bounds for matrix of size " << A.nrows() << "x" << A.ncolumns() << ".";
-                throw std::invalid_argument(error_log.str());
-            }
-
-            memcpy(this -> M, A[row], A.ncolumns() * sizeof(Element)); //FIX: could raise problems with objects
-        }
+        Vector(const Matrix<Element> &A, uint16_t row) : 
+            Matrix<Element>(A, row, row + 1, 0, A.ncolumns()) {}
         
         Vector() :
             Matrix<Element>() {}
 
-        Element operator[] (unsigned int i) const {
-            if (i >= std::max(this -> rows, this -> columns)) {
+        Element operator() (uint16_t i) const {
+            if (i >= this -> size()) {
                 std::ostringstream error_log;
-                error_log << "Index " << i << " out of bounds for vector of size " << std::max(this -> rows, this -> columns) << ".";
+                error_log << "Index " << i << " out of bounds for vector of size " << this -> size();
                 throw std::invalid_argument(error_log.str());
             }
 
-            return this -> M[i];
+            return (this -> M)[i];
         }
 
-        //Seems like i won't be needing it
-        /*Element& operator[] (unsigned int i) {
-            if (i >= std::max(this -> rows, this -> columns)) {
+        Element& operator() (uint16_t i) {
+            if (i >= this -> size()) {
                 std::ostringstream error_log;
-                error_log << "Index " << i << " out of bounds for vector of size " << std::max(this -> rows, this -> columns) << ".";
+                error_log << "Index " << i << " out of bounds for vector of size " << this -> size();
                 throw std::invalid_argument(error_log.str());
             }
 
-            return this -> M[i];
-        }*/
+            return (this -> M)[i];
+        }
 
         Element operator*(const Vector &v) const {
             
-            if (this -> rows != v.rows) {
+            if (this -> size() != v.size()) {
                 std::ostringstream error_log;
-                error_log << "Incompatible sizes for product between vectors of size " << std::max(this -> rows, this -> columns) << " and " << max(v.rows, v.columns) << ".";
+                error_log << "Incompatible sizes for inner product between vectors of size " << this -> size() << " and " << v.size();
                 throw std::invalid_argument(error_log.str());
             }
 
             Element result = Element();
             
-            for (unsigned int i = 0; i < std::max(this -> rows, this -> columns); i++)
-                result += (*this)[i] * v[i];
+            for (uint16_t i = 0; i < this -> size(); i++)
+                result += (this -> M)[i] * v.M[i];
 
             return result;
         }
 
+        friend std::ostream& operator<<(std::ostream &s, const Vector<Element> &v) {
+
+            uint16_t n = v.size();
+
+            s << "[";
+
+            if (n > 0) {
+                for (uint16_t i = 0; i + 1 < n; i++)
+                    s << v.M[i] << ", ";
+
+                s << v.M[n-1];
+            }
+
+            s << "]" << std::endl;
+
+            return s;
+
+        }
 };
-
-template <typename Element>
-std::ostream& operator<<(std::ostream &s, const Vector<Element> &v) {
-
-    unsigned int n = v.nrows() > v.ncolumns() ? v.nrows(): v.ncolumns();
-
-    s << "[";
-
-    if (n > 0) {
-        for (unsigned int i = 0; i < n-1; i++)
-            s << v[i] << ", ";
-
-        s << v[n-1];
-    }
-
-    s << "]" << std::endl;
-
-    return s;
-
-}
 
 #endif

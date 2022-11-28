0ce77dfb3a503d75dd6f5a4bead33b7c4075bdac
torres98 <lucatorresetti@hotmail.it>
2022-11-28 23:56:28 +0100

    Added sha384 test in C++ lib

--- c++/tests/hash_utils_test.cpp
+++ c++/tests/hash_utils_test.cpp
@@ -10,6 +10,7 @@ int main() {
     unsigned char message[1000];
     memset(message, 0x41, sizeof(message));
 
+
 	std::cout << "SHA256 test: ";
 
     unsigned char digest_sha256[32];
@@ -18,6 +19,13 @@ int main() {
 	assert(memcmp(digest_sha256, expected_sha256, 32) == 0);
     std::cout << "PASSED\n";
 
+    std::cout << "SHA512 test: ";
+
+    unsigned char digest_sha384[48];
+    sha384_string(message, digest_sha384, 1000);
+    
+	assert(memcmp(digest_sha384, expected_sha384, 48) == 0);
+    std::cout << "PASSED\n";
 
     std::cout << "SHA512 test: ";
 

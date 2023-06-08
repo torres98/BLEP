23c9d10c024c2b3820923bdbe34f726e016015f4
torres98 <lucatorresetti@hotmail.it>
2023-06-08 14:24:39 +0200

    Add efficient and progressive verification to rainbow cpp test

--- c++/tests/rainbow_test.cpp
+++ c++/tests/rainbow_test.cpp
@@ -3,6 +3,8 @@
 #include "blep/utils/rand.h"
 #include "blep/schemes/rainbow.h"
 #include "blep/mv_verification/std_ver.h"
+#include "blep/mv_verification/eff_ver.h"
+#include "blep/mv_verification/prog_ver.h"
 
 #define STR_IMPL_(x) #x
 #define STR(x) STR_IMPL_(x)
@@ -13,7 +15,24 @@ char const *message_path = STR(PROJECT_DIR) "/tmp/debug.gdb";
 
 using Rainbow::gf;
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        std::cerr << "Wrong number of arguments." << std::endl;
+        exit(1);
+    }
+
+    uint16_t k = strtoul(argv[1], NULL, 10);
+    uint16_t t = strtoul(argv[2], NULL, 10);
+    
+    if (k > Rainbow::n_polynomials) {
+        std::cerr << "The number of rows of the SVK (" << k << ") can't be bigger than the number of rows of the PK (" << Rainbow::n_polynomials << ")." << std::endl;
+        return 1;
+    } else if (t > k) {
+        std::cerr << "The number of progressive steps (" << t << ") can't be bigger than the number of rows of the SVK (" << k << ")." << std::endl;
+        return 1;
+    }
+
     unsigned char salt[Rainbow::SALT_SIZE];
 
     MatrixDS<gf> PK = Rainbow::get_public_key_from_file(pk_path);
@@ -23,6 +42,21 @@ int main() {
 
     assert(verify_signature(PK, s, u));
     std::cout << "Successfully verified the Rainbow signature." << std::endl;
+
+    // for efficient verification
+    auto [C,  SVK] = offVer(PK, k);
+
+    VectorDS<gf> u_eff = C * u;
+
+    assert(verify_signature(SVK, s, u_eff));
+    std::cout << "Efficiently verified the Rainbow signature." << std::endl;
+
+    // for progressive verification
+    assert(progVer(PK, s, u, t, true) == true);
+    std::cout << "Progressively verified the Rainbow signature." << std::endl;
+
+    assert(progVer(SVK, s, u_eff, t, true));
+    std::cout << "Efficiently and progressively verified the Rainbow signature." << std::endl;
 
     return 0;
 }
